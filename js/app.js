ff7a0d7000b7c5f8756fc579b7a3f94dc75629dd
meshnesh <tonnie.nesh@gmail.com>
2017-01-10 15:33:42 +0300

    added font awesome, images and completed the main home section

--- js/app.js
+++ js/app.js
@@ -8,6 +8,21 @@ $(function(){
     // Returns width of HTML document
     var document = $(document).width();
 
+    $('.nav1 .toggle-nav').click(function(e){
+        $('.nav1 .nav-mobile').addClass('style-mobile').slideToggle('slow');
+        e.preventDefault();
+    });
+
+});
+
+
+$(function(){
+
+    // Returns width of browser viewport
+    var browser = $(window).width();
+    // Returns width of HTML document
+    var document = $(document).width();
+
     $('.nav .toggle-nav').click(function(e){
         $('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
         e.preventDefault();
